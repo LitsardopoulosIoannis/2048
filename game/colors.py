37a8ef1ac2880e1292518919a2f409e8bbce9908
Spoker <72018896+SpokerGR@users.noreply.github.com>
2024-01-16 10:54:11 +0200

    Add files via upload

---
+++ game/colors.py
@@ -0,0 +1,50 @@
+GRID_COLOR = "#a39489"
+EMPTY_CELL_COLOR = "#c2b3a9"
+SCORE_LABEL_FONT = ("Verdama", 24)
+SCORE_FONT = ("Helvetica", 36, "bold")
+GAME_OVER_FONT = ("Helvetica", 48, "bold")
+GAME_OVER_FONT_COLOR = "#ffffff"
+WINNER_BG = "#ffcc00"
+LOSER_BG = "#a39489"
+
+CELL_COLORS = {
+    2: "#fcefe6",
+    4: "#f2e8cb",
+    8: "#f5b682",
+    16: "#f29446",
+    32: "#ff775c",
+    64: "#e64c2e",
+    128: "#ede291",
+    256: "#fce130",
+    512: "#ffdb4a",
+    1024: "#f0b922",
+    2048: "#fad74d"
+}
+
+CELL_NUMBER_COLORS = {
+    2: "#695c57",
+    4: "#695c57",
+    8: "#ffffff",
+    16: "#ffffff",
+    32: "#ffffff",
+    64: "#ffffff",
+    128: "#ffffff",
+    256: "#ffffff",
+    512: "#ffffff",
+    1024: "#ffffff",
+    2048: "#ffffff"
+}
+
+CELL_NUMBER_FONTS = {
+    2: ("Helvectica", 55, "bold"),
+    4: ("Helvectica", 55, "bold"),
+    8: ("Helvectica", 55, "bold"),
+    16: ("Helvectica", 50, "bold"),
+    32: ("Helvectica", 50, "bold"),
+    64: ("Helvectica", 50, "bold"),
+    128: ("Helvectica", 45, "bold"),
+    256: ("Helvectica", 45, "bold"),
+    512: ("Helvectica", 45, "bold"),
+    1024: ("Helvectica", 40, "bold"),
+    2048: ("Helvectica", 40, "bold")
+}
